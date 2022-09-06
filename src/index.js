4dff49a6771c3d9197bc640de32cb8f17a379665
yago <yagoalessio88@gmail.com>
2022-09-06 12:41:42 +0200

    index.js correction

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RatingProvider } from "./context/RatingContext.js";
 
 ReactDOM.render(
-  <RatingProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </RatingProvider>,
-  document.getElementById("root")
+	<RatingProvider>
+		<App />
+	</RatingProvider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
